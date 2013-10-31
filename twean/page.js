429d90624c6a2d3f2315e332909f41d4f486fddd
tekool <tekool@github>
2013-10-31 20:55:01 +0100

    Now the script runs before the page is displayed for better performances.

--- twean/page.js
+++ twean/page.js
@@ -1,9 +1,7 @@
-window.addEventListener("load",function()
+new Function()
 {
 	var style = document.createElement("style");
-	style.appendChild(document.createTextNode(""));
 	document.head.appendChild(style);
-	document.querySelector("head").appendChild( style );
 	style.sheet.addRule(".auto-expanded div.media", 'display:none;');
 	style.sheet.addRule("li[data-item-type='tweet'].open div.media", 'display:inline-block !important');
-});
+}
