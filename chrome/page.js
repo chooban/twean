2c10856be886ea87ecff973d0ba305d091ec48e9
unknown <f.saunier@ILIUS958.meetic.ilius.net>
2013-11-12 10:40:45 +0100

    now removes video

--- chrome/page.js
+++ chrome/page.js
@@ -2,6 +2,7 @@ new Function()
 {
 	var style = document.createElement("style");
 	document.head.appendChild(style);
-	style.sheet.addRule(".auto-expanded div.media", 'display:none;');
+	style.sheet.addRule(".tweet .media > .media-thumbnail.is-preview > img", 'display:none;');
+	style.sheet.addRule(".js-stream-tweet > .content > .expanded-content > .tweet-details-fixer > .js-media-container[data-card2-name='player']", 'display:none;');
 	style.sheet.addRule("li[data-item-type='tweet'].open div.media", 'display:inline-block !important');
 }
