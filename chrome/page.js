970f87baed8021f847b6c8d2b0d075fc61f5b309
tekool <tekool@github>
2013-11-12 11:36:17 +0100

    now remove promoted tweets and trends

--- chrome/page.js
+++ chrome/page.js
@@ -2,7 +2,15 @@ new Function()
 {
 	var style = document.createElement("style");
 	document.head.appendChild(style);
-	style.sheet.addRule(".tweet .media > .media-thumbnail.is-preview > img", 'display:none;');
-	style.sheet.addRule(".js-stream-tweet > .content > .expanded-content > .tweet-details-fixer > .js-media-container[data-card2-name='player']", 'display:none;');
-	style.sheet.addRule("li[data-item-type='tweet'].open div.media", 'display:inline-block !important');
+
+	style.sheet.addRule(".js-stream-item .media > .media-thumbnail.is-preview > img", 'display:none;');
+	style.sheet.addRule(".js-stream-item > .content > .expanded-content > .tweet-details-fixer > .js-media-container[data-card2-name='player']", 'display:none;');
+
+	style.sheet.addRule(".js-stream-item.open  .media > .media-thumbnail.is-preview > img", 'display:inline-block !important');
+	style.sheet.addRule(".js-stream-item.open > .content > .expanded-content > .tweet-details-fixer > .js-media-container[data-card2-name='player']", 'display:inline-block !important;');
+
+
+	style.sheet.addRule(".module.trends .promoted", 'display:none;');
+	style.sheet.addRule(".js-stream-item .content .js-action-profile-promoted", 'display:none;');
+
 }
